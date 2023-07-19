b19037db047159a8ab280a5feec4f2f52dd53647
sflorek01 <sflorek01@gmail.com>
2023-07-19 17:57:09 -0400

    sean decode function with implementation

--- armando_lab6.py
+++ armando_lab6.py
@@ -12,6 +12,12 @@ def encode_password(password):
         encoded_password += new_digit
     return encoded_password
 
+def decode_password(password): # added by sean florek
+    decoded_password = ""
+    for number in password:
+        new_digit = str((int(number) - 3) % 10)
+        decoded_password += new_digit
+    return decoded_password
 
 def main():
     print_menu()
@@ -20,8 +26,9 @@ def main():
         password = (input(f"Please enter your password to encode: "))
         encoded_password = encode_password(password)
         print("Your password has been encoded and stored!")
-    elif option == "2":
-        pass
+    elif option == "2":  # added by sean florek
+        decoded_password = decode_password(encoded_password)
+        print(f"The encoded password is {encoded_password} and the original password is {decoded_password}.")
     elif option == "3":
         exit()
 
