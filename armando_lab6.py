b2993536f84cae84f7065de3bdb3118ece32b4bf
Armando Cruz <armandocruz0310@gmail.com>
2023-07-19 18:17:10 -0400

    last commit

--- armando_lab6.py
+++ armando_lab6.py
@@ -20,17 +20,18 @@ def decode_password(password): # added by sean florek
     return decoded_password
 
 def main():
-    print_menu()
-    option = input(f"Please enter an option: ")
-    if option == "1":
-        password = (input(f"Please enter your password to encode: "))
-        encoded_password = encode_password(password)
-        print("Your password has been encoded and stored!")
-    elif option == "2":  # added by sean florek
-        decoded_password = decode_password(encoded_password)
-        print(f"The encoded password is {encoded_password} and the original password is {decoded_password}.")
-    elif option == "3":
-        exit()
+    while True:
+        print_menu()
+        option = input(f"Please enter an option: ")
+        if option == "1":
+            password = (input(f"Please enter your password to encode: "))
+            encoded_password = encode_password(password)
+            print("Your password has been encoded and stored!")
+        elif option == "2":  # added by sean florek
+            decoded_password = decode_password(encoded_password)
+            print(f"The encoded password is {encoded_password} and the original password is {decoded_password}.")
+        elif option == "3":
+            exit()
 
 
 if __name__ == '__main__':
